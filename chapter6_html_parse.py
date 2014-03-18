1d3efb67d730fb6bb37d25ccf9bf8ebf817bbee3
georstef <georstef@gmail.com>
2014-03-18 19:02:17 +0200

    chapter6 (html parse)

---
+++ chapter6_html_parse.py
@@ -0,0 +1,57 @@
+from urllib.request import urlopen
+from urllib.parse import urlparse
+import re
+import sys
+
+LINK_REGEX = re.compile("<a [^>]*href=['\"]([^'\"]+)['\"][^>]*>")
+
+class LinkCollector:
+    def __init__(self, url):
+        self.url = "http://"+urlparse(url).netloc
+        #self.collected_links = set()
+        self.collected_links = {}
+        self.visited_links = set()
+        #print(self.url)
+
+    def collect_links(self, path="/"):
+        full_url = self.url + path
+        self.visited_links.add(full_url)
+        #print(full_url)
+        
+        page = str(urlopen(full_url).read())
+        #print(page)        
+        links = LINK_REGEX.findall(page)
+        links = {self.normalize_url(path, link) for link in links}
+        #print('--',links)
+
+        #self.collected_links = links.union(self.collected_links)
+        self.collected_links[full_url] = links
+        
+        for link in links:
+            self.collected_links.setdefault(link, set())
+
+        unvisited_links = links.difference(self.visited_links)
+        for link in unvisited_links:
+            if link.startswith(self.url):
+                self.collect_links(urlparse(link).path)
+                
+
+    def normalize_url(self, path, link):
+        if link.startswith('http://'):
+            return link
+        elif link.startswith('/'):
+            return self.url + link
+        elif link.startswith('../'):
+            return self.url + link[2:]
+        else:
+            return self.url + path.rpartition('/')[0] + '/'+link
+    
+
+if __name__ == "__main__":
+    collector = LinkCollector('http://localhost:8000')
+    collector.collect_links()
+    #for link in collector.collected_links:
+    #    print(link)
+    for link, item in collector.collected_links.items():
+        print("{}: {}".format(link, item))
+        
